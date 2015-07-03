d7c82a95c7745cfd391fbc13e6a333a9daaf76a9
tarasii <tarasii@gmail.com>
2015-07-03 16:08:39 +0300

    Create pm.py

---
+++ pm.py
@@ -0,0 +1,19 @@
+class pm:
+    def __init__(self):
+        self.dt1 = datetime.datetime.now()
+        self.dt2 = self.dt1
+
+    def __str__(self):
+        if self.dt1 == self.dt2:
+            self.dt2 = datetime.datetime.now()
+        return str(self.delta())
+
+    def start(self):  
+        self.dt1 = datetime.datetime.now()
+
+    def stop(self):
+        self.dt2 = datetime.datetime.now()
+        return self
+        
+    def delta(self):
+        return self.dt2 - self.dt1
