53bcd0625867645e82f76ae8a76a668a1a7450a8
tarasii <tarasii@gmail.com>
2015-07-03 18:24:41 +0300

    optization for functional

--- ex_9_8.py
+++ ex_9_8.py
@@ -26,22 +26,11 @@ def classic_check():
         print i
       i = i + 1
 
-def funktional_check():   
-   #generate list of numbers
+def functional_check():   
    nm = ["%06d" % x for x in range(1000000)]
+   pl = filter(lambda x: check(x), nm)
+   print pl
+   
 
-   #filtering 6 polinoms
-   pl = filter(lambda x: is_palindrome(x), nm)
-
-   #check loop
-   for x in pl:
-      tmp = int(x)
-      if tmp > 4:
-         pl1 = "%06d" % (tmp - 1)
-         pl2 = "%06d" % (tmp - 2)
-         pl3 = "%06d" % (tmp - 3)
-         if is_palindrome(pl1[1:5]) and is_palindrome(pl2[1:6]) and is_palindrome(pl3[2:6]):
-            print pl3, pl2, pl1, x 
-
- standart_check()
- funktional_check()
+standart_check()
+functional_check()
