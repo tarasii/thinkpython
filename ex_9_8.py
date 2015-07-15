0d5c57345457b24173ca2648cbebf0897f5ec313
tarasii <tarasii@gmail.com>
2015-07-16 01:39:02 +0300

    mystr module

--- ex_9_8.py
+++ ex_9_8.py
@@ -1,38 +1,26 @@
-def is_palindrome_classic(word):
-  i = 0
-  j = len(word)-1
-  while i<j:
-    if word[i] != word[j]:
-      return False
-    i = i+1
-    j = j-1
-  return True
-
-def is_palindrome(word):
-  return word[::-1] == word
+import mystr
 
 def check(i):
     pl = "%06d" % (i)
     pl1 = "%06d" % (i+1)
     pl2 = "%06d" % (i+2)
     pl3 = "%06d" % (i+3)
-    return (is_palindrome(pl[2:6])   and
-            is_palindrome(pl1[1:6]) and
-            is_palindrome(pl2[1:5]) and
-            is_palindrome(pl3))
+    return (mystr.is_palindrome(pl[2:6])   and
+            mystr.is_palindrome(pl1[1:6]) and
+            mystr.is_palindrome(pl2[1:5]) and
+            mystr.is_palindrome(pl3))
 
-def classic_check():
+def check_classic():
    i = 0
    while i <= 999996:
       if check(i):
         print i
       i = i + 1
 
-def functional_check():   
-   nm = ["%06d" % x for x in range(1000000)]
+def check_func():   
+   nm = [x for x in range(999997)]
    pl = filter(lambda x: check(x), nm)
    print pl
-   
 
-standart_check()
-functional_check()
+check_classic()
+check_func()
