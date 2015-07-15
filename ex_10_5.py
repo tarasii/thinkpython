cbdbbed7a1f4cd4aee3bbf376fb6fa5214751e3a
tarasii <tarasii@gmail.com>
2015-07-15 19:46:59 +0300

    Update ex_10_5.py

--- ex_10_5.py
+++ ex_10_5.py
@@ -11,20 +11,27 @@ def has_duplicates_classic(l):
 def has_duplicates(l):
     return len(set(l))<>len(l)
 
+def random_day_generation_classic(m):
+    l=[]
+    for i in range(m):
+        l.append(random.randint(1,365))
+    return l
+
+def random_day_generation_func(m):
+    return map(lambda(x):random.randint(1,365),range(m))
+
+def random_day_generation_list(m):
+    return [random.randint(1,365) for x in range(m)]
+
 def two_birthday_probability_classic(n,m):
     cnt = 1
     for j in range(0,n):
-        l=[]
-        for i in range(m):
-            l.append(random.randint(1,365))
-        #l = map(lambda(x):random.randint(1,365),range(m))
-        #l = [random.randint(1,365) for x in range(m)]
-        if has_duplicates(l):
+        if has_duplicates(random_day_generation_classic(m)):
             cnt += 1
     return cnt
 
 def random_list_has_duplicates(m):
-    return has_duplicates([random.randint(1,365) for z in range(m)])
+    return has_duplicates(random_day_generation_list(m))
 
 def two_birthday_probability_func(n,m):
     return reduce(lambda x,y: x + 1 if random_list_has_duplicates(m) else x, range(0,n), 0)
