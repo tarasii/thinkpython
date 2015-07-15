41acaf9c98aa05ecacf79dbb4fd0b69485f908bd
tarasii <tarasii@gmail.com>
2015-07-16 01:35:50 +0300

    Create mystr.py

---
+++ mystr.py
@@ -0,0 +1,69 @@
+def has_letters(word, letters):
+  for letter in word:
+    if letter in letters:
+      return True
+  
+  return False 
+
+def is_palindrome(word):
+  if is_reverse(word,word):
+    return True
+  else:
+    return False
+  
+def is_palindrome_classic(word):
+ i=0
+ j = len(word)-1
+ while i<j:
+   if word[i] != word[j]:
+     return False
+   i += i
+   j -= j
+ return True
+
+def is_reverse(word1, word2):
+  if len(word1) != len(word2):
+     return False
+  if word1 == word2[::-1]:
+     return True
+  return False
+
+def is_reverse_classic(word1, word2):
+   if len(word1) != len(word2):
+      return False
+   i=0
+   j = len(word2)
+   while j > 0:
+      if word1[i] != word2[j]:
+         return False
+         i += i
+         j -= j
+         
+   return True
+
+def file_to_list_simple():
+  fin = open("113809of.fic")
+  t = list(fin)
+  fin.close()  
+  return t
+
+
+def file_to_list_classic():
+  t = []
+  fin = open("113809of.fic")
+  for line in fin:
+    word = line.strip()
+    t.append(word)
+
+  fin.close()
+  return t
+
+def file_to_list_dump():
+  t = []
+  fin = open("113809of.fic")
+  for line in fin:
+    word = line.strip()
+    t = t + [word]
+
+  fin.close()
+  return t
