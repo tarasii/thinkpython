ac67788df913cefc0dd16c6df94ba66432818bcd
tarasii <tarasii@gmail.com>
2015-07-22 19:20:27 +0300

    Update ex_9_2_no_e.py

--- ex_9_2_no_e.py
+++ ex_9_2_no_e.py
@@ -3,20 +3,10 @@ import pm
 
 l = mystr.file_to_list_classic("113809of.fic")
 
-def words_no_letters(wocab, letters):
-  res = []
-  for x in wocab:
-  if not mystr.has_letters(x, letters):
-    res.append(x)
-  return res
-
-def words_no_letters_func(wocab, letters):
-  return filter(lamda a: not mystr.has_letters(a, letters), wocab)
-
 letters = "e"
   
 z = pm.pm()
-rl = words_no_letters(l, letters)
+rl = mystr.words_no_letters(l, letters)
 print z
 
 cnt = len(l)
@@ -27,7 +17,7 @@ print "total words %d, words with no %s %d, this is %d%%" % (cnt,letters,cnt_e,p
 
 
 z.start()
-rl = words_no_letters_func(l,"e")
+rl = mystr.words_no_letters_func(l,"e")
 print z
 
 cnt = len(l)
