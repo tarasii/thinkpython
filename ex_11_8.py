99ab0af70ba063369ab49a12a24514f00ac19f6b
tarasii <tarasii@gmail.com>
2015-08-27 20:50:35 +0300

    to functions

--- ex_11_8.py
+++ ex_11_8.py
@@ -14,42 +14,54 @@ def rotate_word(word, n):
   res = ""
   for x in word.lower():
     t = ord(x) + n
-    if t > z:
-      t = t - z + a - 1
-    elif t < a:
-      t = t + z - a + 1
+    if t > letter_z:
+      t = t - letter_z + letter_a - 1
+    elif t < letter_a:
+      t = t + letter_z - letter_a + 1
     #rint x, ord(x), t
 
     res += chr(t)
     
   return res
 
-a = ord("a")
-z = ord("z")
-y = z - a + 1
+def list_to_dic(w):
+  d = {}
+  for x in w:
+    d[x] = []
+    
+  return d
+  
+def find_rotate_pairs(l, d):
+  for x in l:
+    for n in range(1, y):
+      r = rotate_word(x, n)
+      if r in d:
+        print x, r  
+        d[x].append(r)
+        
+  return none
+  
+def print_pairs(d):
+  for x in l:
+    if len(d[x])>0:
+      print x, d[x]
+
+letter_a = ord("a")
+letter_z = ord("z")
+y = letter_z - letter_a + 1
   
 zz = pm.pm()
 l = mystr.file_to_list_classic("113809of.fic")
 #d = file_to_dict_classic("113809of.fic")
+d = list_to_dic(l)
 print zz
 
-d = {}
-for x in l:
-  d[x] = []
 
 zz.start()
-for x in l:
-  for n in range(1, y):
-    r = rotate_word(x, n)
-    if r in d:
-      print x, r  
-      d[x].append(r)
+find_rotate_pairs(l, d)
 
 print zz
 
 zz.start()
-for x in l:
-  if len(d[x])>0:
-    print x, d[x]
-
+print_pairs(d)
 print zz
