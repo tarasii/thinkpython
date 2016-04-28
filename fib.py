eb9fa73e79f9a262b2d6e631ae11968078932668
tarasii <tarasii@gmail.com>
2016-04-28 16:36:08 +0300

    generator function

--- fib.py
+++ fib.py
@@ -5,6 +5,7 @@ c = 0;
 def fib(n):
   global c 
   c +=1
+
   if n == 0:
     return 0
   elif n == 1:
@@ -19,18 +20,24 @@ def fib(n):
 
 
 def fib_range(n):
+  a,b = 0,1
   if n>0:
-    a,b = 0,1
-    l = range(n)
-    for i in l:
-      a,b = b, a+b
+    for i in range(n):
+      a,b = b,a+b
   else:
-    a,b = 0,1
-    l = range(0, n, -1)
-    for i in l:
-      a,b = b, a-b
+    for i in range(0, n, -1):
+      a,b = b,a-b
 
   return a
+
+
+def fib_generator():
+    a,b = 0,1
+    yield a
+    yield b
+    while True:
+        a, b = b, a + b
+        yield b
 
 
 n = 10
@@ -40,6 +47,13 @@ print pm
 pm.start()
 print n, fib_range(n)
 print pm
+pm.start()
+fb = fib_generator()
+for x in range(n):
+  fb.next()
+print n, fb.next()
+print pm
+
 
 for x in range(10):
    c = 0
