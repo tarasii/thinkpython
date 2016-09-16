d5ab92415a360c7227d516760e0832eefefb6aec
tarasii <tarasii@gmail.com>
2016-09-16 14:03:49 +0300

    fix

--- funktional.py
+++ funktional.py
@@ -2,22 +2,29 @@
 def _factorial(n):
   return reduce(lambda x,y: x*y, [1]+range(1,n+1))
   
-def _simple(n)
-  return reduce(lambda x,y: x and bool(n % y), range(2,n), True)
-
-def factorial_list(l):
+def factorial_list(n):
   #return map(lambda n: reduce(lambda x,y: x*y, [1]+range(1,n+1)),l)
-  return map(_factorial, l)
+  return map(_factorial, range(1,n))
 
+
+def _simple(n):
+  return reduce(lambda x,y: x and bool(n % y), range(2,n), True)
+  
+def simple_list(n):
+  return filter(lambda x: _simple(x), range(2,n))
+
+  
 def random_fill():
   return [random.randint(0,100) for x in [None]*6]
   #return map(lambda x: random.randint(0,100), [None] * 6)
+
 
 def _min(l):
   return reduce(lambda x,y: y if x>y else x,l)
 
 def _max(l):
   return reduce(lambda x,y: y if x<y else x,l)
+
 
 def anti_vowel(s):
   return filter(lambda x: not x in 'aeiuoAEIUO', s)
