8361d8f0acf5733b2e8adad4139dbdc5af5306f4
tarasii <tarasii@gmail.com>
2018-04-20 17:10:03 +0300

    Update pm.py
    new execution time logger

--- pm.py
+++ pm.py
@@ -4,18 +4,24 @@ class pm:
     def __init__(self):
         self.dt1 = datetime.datetime.now()
         self.dt2 = self.dt1
+        self.dt3 = self.dt1
 
     def __str__(self):
-        if self.dt1 >= self.dt2:
-            self.dt2 = datetime.datetime.now()
-        return str(self.delta())
-
-    def start(self):  
-        self.dt1 = datetime.datetime.now()
-
-    def stop(self):
-        self.dt2 = datetime.datetime.now()
-        return self
+        return "{}: {}".format(self.dt2, str(self.delta()))
         
-    def delta(self):
-        return self.dt2 - self.dt1
+    def delta(self, type=0):
+        if type == 0:
+            return self.dt2 - self.dt1
+        else:
+            return self.dt2 - self.dt3
+
+    def reset(self):
+        self.dt3 = self.dt2
+        self.dt2 = datetime.datetime.now()
+
+    def msg(self, txt=""):
+        print txt, self.dt2, self.delta(), self.delta(1)
+
+    def cnt(self, txt=""):
+        self.reset()
+        self.msg(txt)
